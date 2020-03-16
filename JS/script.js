59c03bb8bace132971b8948839901df1403867f6
Kisel_Genya <kiselek.genya@gmail.com>
2020-03-16 21:04:03 +0300

    customization tabs in page

--- JS/script.js
+++ JS/script.js
@@ -21,7 +21,17 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    info.addEventListener('click', function() {
-        
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+
+        if (target && target.classList.contains("info-header-tab")) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
     });
 });
